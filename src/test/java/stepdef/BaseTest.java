cc9fd00611036140471a6d922699d1281c770388
Tania Syawaliyah Putri <tania_ojt@solvera.id>
2026-06-22 16:05:31 +0700

    Add headless Chrome configuration

--- src/test/java/stepdef/BaseTest.java
+++ src/test/java/stepdef/BaseTest.java
@@ -11,8 +11,10 @@ public class BaseTest {
     public static void setupDriver() {
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-notifications");
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
 
